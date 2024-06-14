f08cc46df958c2f3785b4b841754a796cbe145b3
Jae Lo Presti [j4] <git@777.tf>
2024-06-14 12:36:17 +0300

    chore: add repo link to mod meta

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -10,6 +10,7 @@ namespace HeadlessPrometheusExporter
         public override string Name => "Headless Prometheus Exporter";
         public override string Author => "Jae \"J4\" Lo Presti";
         public override string Version => "1.0.0";
+        public override string Link => "https://g.j4.lc/general-stuff/resonite/headless-prometheus-exporter";
 
         private static ModConfiguration _modConf;
 
