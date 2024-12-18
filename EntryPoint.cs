9d18dece164e1908a8b90b84514d9ce927046e3b
Jae Lo Presti <j@j4.lc>
2024-12-18 14:36:45 +0000

    Upgrade to .NET 9

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -9,7 +9,7 @@ namespace HeadlessPrometheusExporter
     {
         public override string Name => "Headless Prometheus Exporter";
         public override string Author => "Jae \"J4\" Lo Presti";
-        public override string Version => "1.2.1";
+        public override string Version => "1.2.2";
         public override string Link => "https://g.j4.lc/general-stuff/resonite/headless-prometheus-exporter";
 
         public static ModConfiguration ModConf { get; private set; }
